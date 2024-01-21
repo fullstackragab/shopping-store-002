c30083466ca9d0b9047c8f322edc2a0f232b1b38
fs-repos <156995325+fs-repos@users.noreply.github.com>
2024-01-21 03:25:59 +0200

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TopBarComponent } from './layout/top-bar/top-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TopBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
