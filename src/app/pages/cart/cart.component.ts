55eda0898ebd4ef2bfde8e5a7d415187ceafbee5
fs-repos <156995325+fs-repos@users.noreply.github.com>
2024-01-23 07:07:21 +0200

    Update

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -44,9 +44,13 @@ export class CartComponent {
     };
 
     this.http
-      .post('http://localhost:8000/api/create-checkout-session', body, {
-        headers: headers,
-      })
+      .post(
+        'https://node-shopping-store.vercel.app/api/create-checkout-session',
+        body,
+        {
+          headers: headers,
+        }
+      )
       .subscribe({
         next: async (response) => {
           const session = response as any;
